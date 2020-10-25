fc04ca79bf0266acf37ee38b3b59911daf447e97
andrewp-as-is <russianidiot.github@gmail.com>
2020-10-25 22:37:44 +0300

    2020:10:25 22:37

--- README.md
+++ README.md
@@ -12,62 +12,18 @@ https://readme42.com
 $ [sudo] pip install django-configurations-base
 ```
 
-#### Features
-key | default value | env
--|-|-
-`ALLOWED_HOSTS` | `['*']` (if `DEBUG=True`) | `DJANGO_ALLOWED_HOSTS`
-`AUTHENTICATION_BACKENDS` | `['django.contrib.auth.backends.ModelBackend']` | `DJANGO_AUTHENTICATION_BACKENDS`
-`AUTH_USER_MODEL` | `auth.User` | `DJANGO_AUTH_USER_MODEL`
-`DATABASES` | `[]` |  `DJANGO_DATABASE_URL`, [dj-database-url](https://github.com/jacobian/dj-database-url)
-`DEBUG` | | `DJANGO_DEBUG` `('yes', 'y', 'true', '1')`
-`INSTALLED_APPS` | [django-configurations-apps](https://pypi.org/project/django-configurations-apps/) | `DJANGO_INSTALLED_APPS`, `DJANGO_APPS_FILE`, `DJANGO_APPS_FIND`, [django-configurations-apps](https://pypi.org/project/django-configurations-apps/)
-`INTERNAL_IPS` | `[]`  | `DJANGO_INTERNAL_IPS`
-`LANGUAGE_CODE` | `en-us`  | `DJANGO_LANGUAGE_CODE`
-`LOGIN_REDIRECT_URL` | `/`  | `DJANGO_LOGIN_REDIRECT_URL`
-`LOGOUT_REDIRECT_URL` | `/`  | `DJANGO_LOGOUT_REDIRECT_URL`
-`MEDIA_URL` | `/media/`  | `DJANGO_MEDIA_URL`
-`MEDIA_ROOT` | `os.path.join(BASE_DIR, 'media')` | `DJANGO_MEDIA_ROOT`
-`MIDDLEWARE` | `MIDDLEWARE_FILE` | `DJANGO_MIDDLEWARE_FILE`
-`ROOT_URLCONF` | `urls` | `DJANGO_ROOT_URLCONF`
-`SECRET_KEY` | | `DJANGO_SECRET_KEY`
-`SITE_ID` | | `DJANGO_SITE_ID`
-`STATIC_URL` | `/static/` | `DJANGO_STATIC_URL`
-`STATIC_ROOT` | `os.path.join(BASE_DIR, 'static')` | `DJANGO_STATIC_ROOT`
-`STATICFILES_DIRS` | `os.path.join(BASE_DIR, 'assets')` (if `./assets/` exists) | `DJANGO_STATICFILES_DIRS`
-`TEMPLATES` | [django-configurations-templates](https://pypi.org/project/django-configurations-templates/) | [django-configurations-templates](https://pypi.org/project/django-configurations-templates/)
-`TIME_ZONE` | `UTC` | `DJANGO_TIME_ZONE`
-`WSGI_APPLICATION` | `wsgi.application` | `DJANGO_WSGI_APPLICATION`
-
-
-`INSTALLED_APPS` ([django-configurations-apps](https://pypi.org/project/django-configurations-apps/))
-
-key  | default value | env
--|-|-
-`INSTALLED_APPS` | `[]` | `DJANGO_INSTALLED_APPS`
-`INSTALLED_APPS_FILE` | `None` | `DJANGO_INSTALLED_APPS_FILE`
-`INSTALLED_APPS_FIND` | `True` | `DJANGO_INSTALLED_APPS_FIND` ('yes', 'y', 'true', '1')
-
-`TEMPLATES` ([django-configurations-templates](https://pypi.org/project/django-configurations-templates/))
-
-key  | default value | env
--|-|-
-`TEMPLATES_BACKEND` | `django.template.backends.django.DjangoTemplates` | `DJANGO_TEMPLATES_BACKEND`
-`TEMPLATES_DIRS` | `os.path.join(BASE_DIR,'templates')` | `DJANGO_TEMPLATES_DIRS`
-`TEMPLATES_APP_DIRS` | `True` |`DJANGO_TEMPLATES_APP_DIRS` ('yes', 'y', 'true', '1')
-`TEMPLATES_OPTIONS` | |
-`TEMPLATES_CONTEXT_PROCESSORS` | `[]` | `DJANGO_TEMPLATES_CONTEXT_PROCESSORS`
-`TEMPLATES_CONTEXT_PROCESSORS_FILE` | `None` | `DJANGO_TEMPLATES_CONTEXT_PROCESSORS_FILE`
-`TEMPLATES_LOADERS` | `[]` |
-
 ##### `settings.py`
 ```python
 from django_configurations_base import BaseConfiguration
 
-class Base(BaseConfiguration,...):
-    APPS_FILE = 'apps.txt'
-    APPS_FIND = True
-    MIDDLEWARE_FILE = 'middleware.txt'
-    TEMPLATES_CONTEXT_PROCESSORS_FILE = 'context_processors.txt'
+class Base(BaseConfiguration):
+    ...
+```
+
+##### `.env`
+```bash
+DJANGO_STATIC=/static/
+...
 ```
 
 #### Links
--- django_configurations_base/__init__.py
+++ django_configurations_base/__init__.py
@@ -1,62 +1,132 @@
 import os
 
-from configurations import values
-from django_configurations_apps import AppsConfiguration
-from django_configurations_templates import TemplatesConfiguration
+from django.conf.global_settings import *
+from configurations import Configuration, values
 
-def getlist(path):
-    return list(filter(
-        None,map(lambda s:s.strip(),open(path).read().splitlines() if path else [])
-    ))
 
-class BaseConfiguration(AppsConfiguration,TemplatesConfiguration):
-    ALLOWED_HOSTS = values.ListValue([])
-    AUTHENTICATION_BACKENDS = values.ListValue(['django.contrib.auth.backends.ModelBackend'])
-    AUTH_USER_MODEL = values.Value('auth.User')
+class BaseConfiguration(Configuration):
     BASE_DIR = os.getcwd()
-    DEBUG = bool(os.getenv('DJANGO_DEBUG','').lower() in ('yes', 'y', 'true', '1'))
-    INTERNAL_IPS = values.ListValue([])
-    LANGUAGE_CODE = values.Value('en-us')
-    LOGIN_REDIRECT_URL = values.Value('/')
-    LOGOUT_REDIRECT_URL = values.Value('/')
-    MEDIA_ROOT = values.Value(None)
-    MEDIA_URL = '/media/'
-    MIDDLEWARE = values.ListValue([])
-    MIDDLEWARE_FILE = values.Value(None)
     ROOT_URLCONF = values.Value(None)
     SITE_ID = values.IntegerValue(None)
-    STATIC_ROOT = values.Value(None)
-    STATIC_URL = '/static/'
+
+    # CORE
+    DEBUG = values.BooleanValue(DEBUG)
+    DEBUG_PROPAGATE_EXCEPTIONS = values.BooleanValue(
+        DEBUG_PROPAGATE_EXCEPTIONS)
+    INTERNAL_IPS = values.ListValue(INTERNAL_IPS)
+    ALLOWED_HOSTS = values.ListValue(ALLOWED_HOSTS)
+    TIME_ZONE = values.Value(TIME_ZONE)
+    USE_TZ = values.Value(USE_TZ)
+    LANGUAGE_CODE = values.Value(LANGUAGE_CODE)
+    LANGUAGES_BIDI = values.Value(LANGUAGES_BIDI)
+    USE_I18N = values.Value(USE_I18N)
+    LOCALE_PATHS = values.ListValue([])
+    USE_L10N = values.Value(USE_L10N)
+    DEFAULT_CHARSET = values.Value(DEFAULT_CHARSET)
+    SERVER_EMAIL = values.Value(SERVER_EMAIL)
+    DATABASE_ROUTERS = values.Value(DATABASE_ROUTERS)
+    EMAIL_BACKEND = values.Value(EMAIL_BACKEND)
+    EMAIL_HOST = values.Value(EMAIL_HOST)
+    EMAIL_PORT = values.IntegerValue(EMAIL_PORT)
+
+    INSTALLED_APPS = values.ListValue(INSTALLED_APPS)
+    FORM_RENDERER = values.Value(FORM_RENDERER)
+    DISALLOWED_USER_AGENTS = values.Value(DISALLOWED_USER_AGENTS)
+    IGNORABLE_404_URLS = values.Value(IGNORABLE_404_URLS)
+    SECRET_KEY = values.SecretValue(None)
+    DEFAULT_FILE_STORAGE = values.Value(DEFAULT_FILE_STORAGE)
+    MEDIA_ROOT = values.Value(MEDIA_ROOT)
+    MEDIA_URL = values.Value(MEDIA_URL)
+    STATIC_ROOT = values.Value(STATIC_ROOT)
+    STATIC_URL = values.Value(STATIC_URL)
+    FILE_UPLOAD_HANDLERS = values.ListValue(FILE_UPLOAD_HANDLERS)
+    FILE_UPLOAD_MAX_MEMORY_SIZE = values.IntegerValue(
+        FILE_UPLOAD_MAX_MEMORY_SIZE)
+    DATA_UPLOAD_MAX_MEMORY_SIZE = values.IntegerValue(
+        DATA_UPLOAD_MAX_MEMORY_SIZE)
+    DATA_UPLOAD_MAX_NUMBER_FIELDS = values.IntegerValue(
+        DATA_UPLOAD_MAX_NUMBER_FIELDS)
+    FILE_UPLOAD_TEMP_DIR = values.Value(FILE_UPLOAD_TEMP_DIR)
+
     STATICFILES_DIRS = values.ListValue([])
-    SECRET_KEY = values.SecretValue()
-    TIME_ZONE = values.Value('UTC')
-    WSGI_APPLICATION = values.Value(None)
+
+    WSGI_APPLICATION = values.Value(WSGI_APPLICATION)
+
+    # MIDDLEWARE
+    MIDDLEWARE = values.ListValue([])
+
+    # AUTHENTICATION
+    AUTH_USER_MODEL = values.Value(AUTH_USER_MODEL)
+    AUTHENTICATION_BACKENDS = values.ListValue(AUTHENTICATION_BACKENDS)
+    LOGIN_URL = values.Value(LOGIN_URL)
+    LOGIN_REDIRECT_URL = values.Value(LOGIN_URL)
+    LOGOUT_REDIRECT_URL = values.Value(LOGOUT_REDIRECT_URL)
+    PASSWORD_RESET_TIMEOUT_DAYS = values.IntegerValue(
+        PASSWORD_RESET_TIMEOUT_DAYS)
+    PASSWORD_RESET_TIMEOUT = values.IntegerValue(PASSWORD_RESET_TIMEOUT)
+    PASSWORD_HASHERS = values.ListValue(PASSWORD_HASHERS)
+    AUTH_PASSWORD_VALIDATORS = values.ListValue(AUTH_PASSWORD_VALIDATORS)
+
+    # SIGNING
+    SIGNING_BACKEND = values.Value(SIGNING_BACKEND)
+
+    # CSRF
+    CSRF_FAILURE_VIEW = values.Value(CSRF_FAILURE_VIEW)
+    CSRF_COOKIE_NAME = values.Value(CSRF_COOKIE_NAME)
+    CSRF_COOKIE_AGE = values.IntegerValue(CSRF_COOKIE_AGE)
+    CSRF_COOKIE_DOMAIN = values.Value(CSRF_COOKIE_DOMAIN)
+    CSRF_COOKIE_PATH = values.Value(CSRF_COOKIE_PATH)
+    CSRF_COOKIE_SECURE = values.BooleanValue(CSRF_COOKIE_SECURE)
+    CSRF_COOKIE_HTTPONLY = values.BooleanValue(CSRF_COOKIE_SECURE)
+    CSRF_COOKIE_SAMESITE = values.Value(CSRF_COOKIE_SAMESITE)
+    CSRF_HEADER_NAME = values.Value(CSRF_HEADER_NAME)
+    CSRF_TRUSTED_ORIGINS = values.ListValue(CSRF_TRUSTED_ORIGINS)
+    CSRF_USE_SESSIONS = values.BooleanValue(CSRF_USE_SESSIONS)
+
+    # MESSAGES
+    MESSAGE_STORAGE = values.Value(MESSAGE_STORAGE)
+
+    # LOGGING
+    LOGGING_CONFIG = values.Value(LOGGING_CONFIG)
+    DEFAULT_EXCEPTION_REPORTER = values.Value(DEFAULT_EXCEPTION_REPORTER)
+    DEFAULT_EXCEPTION_REPORTER_FILTER = values.Value(
+        DEFAULT_EXCEPTION_REPORTER_FILTER)
+
+    # TESTING
+    TEST_RUNNER = values.Value(TEST_RUNNER)
+    TEST_NON_SERIALIZED_APPS = values.ListValue(TEST_NON_SERIALIZED_APPS)
+
+    # FIXTURES
+    FIXTURE_DIRS = values.ListValue(FIXTURE_DIRS)
+
+    # STATICFILES
+    STATICFILES_DIRS = values.ListValue(STATICFILES_DIRS)
+    STATICFILES_STORAGE = values.Value(STATICFILES_STORAGE)
+    STATICFILES_FINDERS = values.ListValue(STATICFILES_FINDERS)
+
+    # SYSTEM CHECKS
+    SILENCED_SYSTEM_CHECKS = values.ListValue(SILENCED_SYSTEM_CHECKS)
+
+    # SECURITY MIDDLEWARE
+    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(SECURE_BROWSER_XSS_FILTER)
+    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(
+        SECURE_CONTENT_TYPE_NOSNIFF)
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(
+        SECURE_HSTS_INCLUDE_SUBDOMAINS)
+    SECURE_HSTS_PRELOAD = values.BooleanValue(SECURE_HSTS_PRELOAD)
+    SECURE_HSTS_SECONDS = values.IntegerValue(SECURE_HSTS_SECONDS)
+    SECURE_REDIRECT_EXEMPT = values.ListValue(SECURE_REDIRECT_EXEMPT)
+    SECURE_REFERRER_POLICY = values.Value(SECURE_REFERRER_POLICY)
+    SECURE_SSL_HOST = values.Value(SECURE_SSL_HOST)
+    SECURE_SSL_REDIRECT = values.BooleanValue(SECURE_SSL_REDIRECT)
 
     @classmethod
     def pre_setup(cls):
         super(BaseConfiguration, cls).pre_setup()
         if cls.DEBUG:
             cls.ALLOWED_HOSTS = ['*']
-        if not hasattr(cls,'MEDIA_ROOT') or not cls.MEDIA_ROOT:
-            cls.MEDIA_ROOT = os.path.join(cls.BASE_DIR, 'media')
-        if not hasattr(cls,'STATIC_ROOT') or not cls.STATIC_ROOT:
-            cls.STATIC_ROOT = os.path.join(cls.BASE_DIR, 'static')
-        if not hasattr(cls,'STATICFILES_DIRS') or not cls.STATICFILES_DIRS:
-            path = os.path.join(cls.BASE_DIR, 'assets')
-            if os.path.exists(path):
-                cls.STATICFILES_DIRS = (
-                    path,
-                )
-        if not cls.DATABASES and 'DJANGO_DATABASE_URL' in os.environ:
-            cls.DATABASES = values.DatabaseURLValue(default=os.environ['DJANGO_DATABASE_URL'])
-        if not hasattr(cls,'ROOT_URLCONF') or not cls.ROOT_URLCONF:
-            for filename in ['urls','urls.py']:
-                if os.path.exists(os.path.join(cls.BASE_DIR, filename)):
-                    cls.ROOT_URLCONF = 'urls'
 
-    @classmethod
-    def setup(cls):
-        super(BaseConfiguration, cls).setup()
-        for middleware in getlist(cls.MIDDLEWARE_FILE):
-            if middleware not in cls.MIDDLEWARE:
-                cls.MIDDLEWARE.append(middleware)
+
+"""
+https://github.com/django/django/blob/master/django/conf/global_settings.py
+"""
